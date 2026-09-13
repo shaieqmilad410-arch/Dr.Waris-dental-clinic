f6d1dc2ed32b373489b3934d2e42576ef28a5b27
shaieqmilad410-arch <shaieqmilad410@gmail.com>
2026-09-14 00:07:22 +0430

    Fix FAQ import in Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ import TestimonialsSection from "../Componants/TestimonialsSection";
 import DoctorsSection from "../Componants/DoctorsSection";
 import LocationSection from "../Componants/LocationSection";
 import Footer from "../Componants/Footer";
-import FAQ from "../Componants/Faq";
+import FAQ from "../Componants/FaQ";
 import DeveloperCredit from "../Componants/DeveloperCredit";
 
 const Home = () => {
